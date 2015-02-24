5790e7f33ef56e5537bb2bba29f9def804cf00b7
Mystine Laframboise Vien <mystinelaframboisevien@Mystines-MacBook-Pro.local>
2015-02-23 19:21:24 -0500

    adding javascript

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var $form = $('form');
 var $toDo = $('#to-do');
 
 $form.on('submit'. function () }
+	var $ul = $('<ul>');
+	$('body').append('<li>' + input_value + '<a href="">done</a></li>')
 
 $(document).on('click', 'a'. function (e) {
 	e.preventDefault();
