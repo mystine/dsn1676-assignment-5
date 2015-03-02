b29f025fd9557898faae5f6481f5417ae282e9c6
Mystine Laframboise Vien <mystinelaframboisevien@Mystines-MacBook-Pro.local>
2015-03-02 13:32:00 -0500

    found error

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $form.on('submit', function(eventObject) {
 	var $theLI = $('<li>');
 	$theLI.html($input.val());
 
-	#listField.prepend($theLI);
+	$listField.prepend($theLI);
 
 	$input.val('');
 })
