1cb134a3c7215231063a2d59afb2176ee0df8893
Mystine Laframboise Vien <mystinelaframboisevien@Mystines-MacBook-Pro.local>
2015-03-02 10:26:39 -0500

    changed ul to ol

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ var $toDo = $('#to-do');
 
 $form.on('submit'. function () }
 	var $ul = $('<ul>');
-	$('body').append('<li>' + input_value + '<a href="">done</a></li>')
+	$('body').append($div);
 
 $(document).on('click', 'a'. function (e) {
 	e.preventDefault();
-}
+});
